fb8a3c82f7e6f74aa550813149bf85b99663f448
ABI743 <5amvibezofficial@gmail.com>
2024-10-25 13:55:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    unoptimized: true,
+    domains: ['vercel.com'],
   },
-  basePath: '/Treasured-scribbles',
-  assetPrefix: '/Treasured-scribbles/',
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+  // Uncomment the following lines if you need to handle custom routes
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: '/api/:path*',
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
