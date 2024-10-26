4c6e41f4fb2db8202332e4b71151036c09335f7b
ABI743 <ABI743@users.noreply.github.com>
2024-10-26 03:50:37 +0000

    deploy: 04e432707a014d4755bd9c0e83107bf9677f73b5

---
+++ _next/static/tHWTZ44u3AtiHl8QpS8Vx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{beforeFiles:[],afterFiles:[{source:e,destination:e}],fallback:[]},"/":["static/chunks/pages/index-4dd9ef0d2a568b43.js"],"/_error":["static/chunks/pages/_error-19e73c3be68140bc.js"],sortedPages:["/","/_app","/_error"]}}("/api/:path*"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
